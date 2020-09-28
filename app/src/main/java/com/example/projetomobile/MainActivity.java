f8cc0dde59be82bb08c7b0a2d29872a99da71691
unknown <jioannysaid@gmail.com>
2020-09-27 22:55:31 -0300

    Functional login

--- app/src/main/java/com/example/projetomobile/MainActivity.java
+++ app/src/main/java/com/example/projetomobile/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.projetomobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText tilEmail;
+    EditText tilPassword;
+    ImageButton btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        tilEmail = findViewById(R.id.tilEmail);
+        tilPassword = findViewById(R.id.tilPassword);
+        btLogin = findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    private void doLogin(){
+        String msg = "Iniciando Login";
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+        String email = tilEmail.getText().toString();
+        String password = tilPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+        if(currentUser != null){
+            updateUI(currentUser);
+        }else{
+            String msg = "Erro ao autenticar o usuário";
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateUI(FirebaseUser firebaseUser){
+        String msg = "Iniciando Tela Pricipal do app";
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+        finish();
+        startActivity(new Intent(MainActivity.this, HelloActivity.class));
     }
 }
