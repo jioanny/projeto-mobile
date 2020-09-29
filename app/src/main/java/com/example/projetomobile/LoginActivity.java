556e7c04ffca8921277b6746bd921da2aca104a5
unknown <jioannysaid@gmail.com>
2020-09-28 23:24:47 -0300

    Cadastrar

--- app/src/main/java/com/example/projetomobile/LoginActivity.java
+++ app/src/main/java/com/example/projetomobile/LoginActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -17,20 +16,21 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     EditText tilEmail;
     EditText tilPassword;
     ImageButton btLogin;
+    ImageButton btCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-
+        btCadastrar = findViewById(R.id.btnCadastrar);
         tilEmail = findViewById(R.id.tilEmail);
         tilPassword = findViewById(R.id.tilPassword);
         btLogin = findViewById(R.id.btLogin);
@@ -41,11 +41,18 @@ public class MainActivity extends AppCompatActivity {
                 doLogin();
             }
         });
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CadastrarActivity.class));
+            }
+        });
     }
 
     private void doLogin(){
         String msg = "Iniciando Login";
-        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
 
         String email = tilEmail.getText().toString();
         String password = tilPassword.getText().toString();
@@ -58,31 +65,19 @@ public class MainActivity extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-
-        if(currentUser != null){
-            updateUI(currentUser);
-        }else{
-            String msg = "Erro ao autenticar o usuário";
-            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-        }
-    }
+    
 
     private void updateUI(FirebaseUser firebaseUser){
         String msg = "Iniciando Tela Pricipal do app";
-        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_LONG).show();
         finish();
-        startActivity(new Intent(MainActivity.this, HelloActivity.class));
+        startActivity(new Intent(LoginActivity.this, HelloActivity.class));
     }
 }
